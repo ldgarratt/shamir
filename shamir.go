9c621fa648a9908d6b84ea9fe608721b20786223
Luke Garratt <29069206+ldgarratt@users.noreply.github.com>
2021-11-21 16:55:17 -0500

    Added split capability for arbitrary length secrets
    Now I just need to add the capability to combine arbitrary length secrets and
    tidy up the output

--- shamir.go
+++ shamir.go
@@ -9,7 +9,16 @@ import (
     "math/big"
     "os"
     "strconv"
+    "unicode"
 )
+
+// 2**127 - 1. Cannot declare bigInts as consts to put the string here.
+const PRIME_STRING = "170141183460469231731687303715884105727"
+
+// Split large secrets into smaller ones to avoid wrapping around the prime
+// modulus during encoding. Given we only accept ASCII values, splitting into
+// chunks of size 15 is sufficent.
+const secret_split = 15
 
 // A polynomial is a slice of big.Ints. polynomial[i] is the x^i coefficient.
 // E.g. 7x^2 + 5 is [5, 0, 7].
@@ -128,7 +137,6 @@ func generateRandomPolynomial(constant, modulus *big.Int, degree int) polynomial
             break
         }
     }
-
     return polynomial{coefficients}
 }
 
@@ -199,12 +207,10 @@ func lagrange(points map[int]big.Int, modulus *big.Int) *big.Int {
         term := new(big.Int).Mul(&y, prod)
         result = result.Add(result, term)
     }
-
     return result.Mod(result, modulus)
 }
 
-// TODO: maybe this should just work on ints only.
-// Write tests... write these better too probably
+// TODO: write tests
 func stringToBigInt(s string) *big.Int {
     return new(big.Int).SetBytes([]byte(s))
 }
@@ -217,12 +223,94 @@ func bigIntToString(i *big.Int) string {
     return string(q)
 }
 
+func isASCII(s string) bool {
+    for i := 0; i < len(s); i++ {
+        if s[i] > unicode.MaxASCII {
+            return false
+        }
+    }
+    return true
+}
+
+func validParameters(splitSecret *string, splitn, splitthreshold *int) bool {
+    if *splitSecret == "" {
+        fmt.Println("Empty secret.\nSee README.md for example usage")
+        return false
+    }
+    if !isASCII(*splitSecret) {
+        fmt.Println("Secret must be ASCII.")
+        return false
+    }
+    if *splitn < 1 {
+        fmt.Println("Number of shares less than 1.\nSee README.md for example useage")
+        return false
+    }
+    if *splitthreshold < 1 {
+        fmt.Println("Threshold less than 1.\nSee README.md for example useage")
+        return false
+    }
+    if *splitn < * splitthreshold {
+        fmt.Println("Number of shares is less than the threshold.")
+        return false
+    }
+    return true
+}
+
+// Splits a string into an array of smaller strings, where each element is a
+// string of size <= i. They will all be size n, except the last chunk which
+// might be smaller. No padding required.
+// We use this function to split a large secret into smaller ones, because
+// otherwise secrets would wrap around the modulus during encoding.
+func splitStringIntoChunks(s string, n int) []string {
+    res := []string{}
+    // first chunks of length 5. Spare chunk left over if not exactly multiple
+    // of n
+    for i := 0; i <= len(s) - n; i += n {
+        res = append(res, s[i:i+n])
+    }
+    // Add the spare part if it exists.
+    if len(s) % n != 0 {
+        res = append(res, s[len(res)*n:])
+    }
+    return res
+}
+
+// Takes as input a slice of bigInt slices and pairwise combines them and
+// eturns a slice of strings.
+// We use this when splitting up a secret into individual subsecrets, shamir
+// splitting each one, then re-combining each share into a master share in a
+// useful string format for later parsing.
+// E.g. for a secret which is split into 3 subsecrets and shared among 2 people:
+// subsecret 1 shares: [23, 345]
+// subsecret 2 shares: [100, 99]
+// subsecret 3 shares: [19, 50]
+// Input: [[23, 345], [100, 99], [19, 50]]
+// Returns: [23+100+19, 345+99+50]
+func pairwiseJoinSlices(a [][]*big.Int) []string {
+    inner_slice_len := len(a[0])
+    for _, element := range(a) {
+        if len(element) != inner_slice_len {
+            panic("Slices not all the same length.")
+        }
+    }
+    res := []string{}
+    // Build the jth string
+    // To build the jth string, take the jth element from each inner slice and
+    // add a "+" at the end for each, except the last.
+    for j := 0; j <= inner_slice_len  - 1; j++ {
+        str := ""
+        for i := 0; i < len(a) - 1; i++ {
+            str += a[i][j].String() + "+"
+        }
+        str += a[len(a)-1][j].String()
+        // Add the jth string to result
+        res = append(res, str)
+    }
+    return res
+}
+
 func main() {
-    // 2**127 - 1
-    PRIME := big.NewInt(2)
-    PRIME.Exp(PRIME, big.NewInt(127), nil)
-    PRIME.Sub(PRIME, big.NewInt(1))
-    fmt.Println("The prime is: ", PRIME)
+    PRIME, _ := new(big.Int).SetString(PRIME_STRING, 10)
 
     splitCmd := flag.NewFlagSet("split", flag.ExitOnError)
     splitSecret := splitCmd.String("secret", "", "Secret to split")
@@ -232,24 +320,40 @@ func main() {
     combineCmd := flag.NewFlagSet("combine", flag.ExitOnError)
 
     if len(os.Args) < 2 {
-        fmt.Println("expected 'split' or 'combine' subcommands")
+        fmt.Println("Expected 'split' or 'combine' subcommands.\nSee README.md for example usage.")
         os.Exit(1)
     }
 
     switch os.Args[1] {
         case "split":
             splitCmd.Parse(os.Args[2:])
-            fmt.Println("subcommand 'split'")
-            fmt.Println("split secret", *splitSecret )
-
-            if *splitSecret == "" {
-                fmt.Println("Empty secret")
-                return
+            if !validParameters(splitSecret, splitn, splitthreshold) {
+                os.Exit(1)
             }
+            fmt.Println("Secret to split:", *splitSecret )
 
-            secret := stringToBigInt(*splitSecret)
-            fmt.Println("Secret is: ", secret)
-            shamirSplitSecret(secret, PRIME, *splitn, *splitthreshold)
+            // TODO: put this into a separate function
+            test_secret := splitStringIntoChunks(*splitSecret, secret_split)
+            var result [][]*big.Int
+            for _, subsecret := range test_secret {
+                subsecret_int := stringToBigInt(subsecret)
+                subsecret_shares := shamirSplitSecret(subsecret_int, PRIME, *splitn, *splitthreshold)
+                result = append(result, subsecret_shares)
+                fmt.Println(len(result))
+                fmt.Println("yup")
+            }
+            fmt.Println("wwwwww")
+            fmt.Println(result)
+            shares := pairwiseJoinSlices(result)
+            fmt.Println(shares)
+            fmt.Println("zzzz")
+            fmt.Println(len(shares))
+            fmt.Println("where am i?")
+            for i, _ := range(shares) {
+                fmt.Printf("Share %d: (%d, %s)", i+1, i+1, shares[i])
+            }
+            fmt.Println("where??")
+            fmt.Println(len(shares))
 
         case "combine":
             combineCmd.Parse(os.Args[2:])
@@ -264,7 +368,6 @@ func main() {
             }
             fmt.Println(points)
             secret := lagrange(points, PRIME)
-            // TODO: Maybe just work on ints only?
             fmt.Println(bigIntToString(secret))
         default:
             fmt.Println("Expected 'split' or 'combine' subcommands")
